a4d8aeea41db3a3e4cd0a11e0f915d3712b2f268
Raj <raj.mobio@gmail.com>
2022-02-10 17:57:20 +0530

    commit

--- denish_ivy/denish_ivy/settings.py
+++ denish_ivy/denish_ivy/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'image/'
+
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
 ]
