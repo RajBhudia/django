35951a5613053c18e1ccf8e9b69e2794b1a49eea
Raj <raj.mobio@gmail.com>
2022-02-11 14:38:42 +0530

    commit

--- denish_ivy/account/models.py
+++ denish_ivy/account/models.py
@@ -1,8 +1,3 @@
-from concurrent.futures.process import _threads_wakeups
-from curses.ascii import alt
-from sre_constants import CATEGORY
-from traceback import print_exception
-from unicodedata import category
 from django.db import models
 
 
@@ -14,18 +9,44 @@ class Customer(models.Model):
     email = models.CharField(max_length=100, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
+
 
 class Product(models.Model):
-    CATEGORY = (
-                ('Indor', 'Indor'), 
-                ('Outdoor', 'Outdoor')
-    
-                )
+    CATEGORIES =(
+        ('Indor', 'Indor'),
+        ('Outdor', 'Outdor'),
+    )
     name = models.CharField(max_length=100)
     price = models.FloatField()
-    category = models.CharField(max_length=100, choices=CATEGORY)
+    category = models.CharField(max_length=100, choices=CATEGORIES, default='')
     description = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+        
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS =(
+        ('Pending', 'Pending'),
+        ('out for delivery', 'Out for Delivery'),
+        ('Deliverd', 'Deliverd'),
+    )
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100, choices=STATUS, default='')
+    tag = models.ManyToManyField(Tag)
+
 
 
 
