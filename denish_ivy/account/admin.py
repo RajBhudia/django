def0c3cabe2879d47045e636a0606ab8f5c54449
Raj <raj.mobio@gmail.com>
2022-02-11 12:07:03 +0530

    commit

--- denish_ivy/account/admin.py
+++ denish_ivy/account/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product, Customer 
 
 # Register your models here.
+@admin.register(Customer)
+class customeradmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email', 'date_created')
+
+@admin.register(Product)
+class productadmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description', 'price', 'date_created')
--- denish_ivy/account/models.py
+++ denish_ivy/account/models.py
@@ -1,3 +1,32 @@
+from concurrent.futures.process import _threads_wakeups
+from curses.ascii import alt
+from sre_constants import CATEGORY
+from traceback import print_exception
+from unicodedata import category
 from django.db import models
 
+
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.IntegerField(blank=False)
+    email = models.CharField(max_length=100, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    CATEGORY = (
+                ('Indor', 'Indor'), 
+                ('Outdoor', 'Outdoor')
+    
+                )
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(max_length=100, choices=CATEGORY)
+    description = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+
+
